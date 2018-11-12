cc106f1b11c3e0ccb7829d92bd1ff6bc03b76d5b
Jerome <jerome.david@cegetel.net>
2018-11-12 12:20:52 +0100

    ok

---
+++ main.js
@@ -0,0 +1,29 @@
+/*Ajoute un liseré bleu au dessus du nom de la section de page active dans la barre de navigation*/
+$(document).ready(function() {
+    $('#container-top li').on('click', function(){
+        $('#container-top li').removeClass('active');
+        $(this).toggleClass('active');
+    })
+});
+
+/*Basculement sur le menu burger en mode responsive*/
+$('.burger').click(function(e){
+	$('nav ul').toggleClass('showMenu');
+});
+
+/*Défilement vers la gauche ou la droite du slider*/
+var goRight = true;
+$(function () {
+	$('#arrow-right').on('click', function () {
+		if (goRight){
+			$('#slider figure').css('left', '-=100%');
+			goRight = false;
+		}
+	});
+	$('#arrow-left').on('click', function () {
+		if (!goRight){
+			$('#slider figure').css('left', '+=100%');
+			goRight = true;
+		}
+	});
+});
